c62b5f435a9e0e033bd4a0bbd78b9f4bf44516b3
Your Name <you@example.com>
2022-01-25 18:04:11 -0500

    created the topnavbar and a second navbar below

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TipTopNavBar } from './navigation/tiptopnavbar.component';
 import { TopNavBar } from './navigation/topnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBar
+    TopNavBar,
+    TipTopNavBar
   ],
   imports: [
     BrowserModule
---
+++ src/app/navigation/tiptopnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'dsg-tiptopnavbar',
+    templateUrl: 'tiptopnavbar.component.html',
+    styleUrls: ['tiptopnavbar.component.css']
+})
+export class TipTopNavBar{
+
+}
