242de7f6642ea48e103b9a810a34ea736f78d63a
Your Name <you@example.com>
2022-02-02 19:21:52 -0500

    cloned repo, added first carousel, several buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { firstcarousel } from './categories/firstcarousel.component';
 import { thirdTopNavBar } from './navigation/thirdtopnavbar.component';
 import { TipTopNavBar } from './navigation/tiptopnavbar.component';
 import { TopNavBar } from './navigation/topnavbar.component';
+import { CardComponent } from './categories/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBar,
     TipTopNavBar,
-    thirdTopNavBar
+    thirdTopNavBar,
+    CardComponent,
+    firstcarousel
   ],
   imports: [
     BrowserModule
---
+++ src/app/categories/firstcarousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'dsg-first-carousel',
+    templateUrl: 'firstcarousel.component.html',
+    styleUrls: ['firstcarousel.component.css']
+})
+
+export class firstcarousel{
+    
+}
